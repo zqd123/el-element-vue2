a87cfebd678302dfc8d0e72e5fb5d0f67c305649
zqd123 <9544850405@qq.com>
2024-08-02 21:51:20 +0800

    feat: 表单嵌套表格

---
+++ src/components/FormTable.vue
@@ -0,0 +1,71 @@
+<template>
+    <div>
+
+        <el-form
+          :model="ruleForm"
+          :rules="rules"
+          ref="ruleForm"
+          label-width="100px"
+          class="demo-ruleForm"
+        >
+          <el-table :data="ruleForm.tableData" style="width: 100%">
+            <el-table-column prop="date" label="日期" >
+              <template slot-scope="scope">
+                <el-form-item prop="name">
+                  <el-input v-model="scope.row.name"></el-input>
+                </el-form-item>
+              </template>
+            </el-table-column>
+            <el-table-column  label="姓名" >
+              <template slot-scope="scope">
+                <el-form-item :prop="'tableData.'+scope.$index+'.age'" :rules="rules.age">
+                  <el-input v-model="scope.row.age"></el-input>
+                </el-form-item>
+              </template>
+            </el-table-column>
+          </el-table>
+        </el-form>
+        <el-button type="primary" @click="submitForm('ruleForm')">提交</el-button>
+    </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      ruleForm: {
+        tableData: [
+          {
+            name: "",
+            age: "",
+          },
+        ],
+      },
+      rules: {
+        name: [
+          { required: true, message: "请输入活动名称", trigger: "blur" },
+          { min: 3, max: 5, message: "长度在 3 到 5 个字符", trigger: "blur" },
+        ],
+        age: [{ required: true, validate: this.verifyAge, trigger: "blur" }],
+      },
+    };
+  },
+  methods: {
+    verifyAge(rule, value, callback) {
+      console.log({ rule, value, callback });
+    },
+    submitForm(formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          alert("submit!");
+        } else {
+          console.log("error submit!!");
+          return false;
+        }
+      });
+    },
+    resetForm(formName) {
+      this.$refs[formName].resetFields();
+    },
+  },
+};
+</script>
