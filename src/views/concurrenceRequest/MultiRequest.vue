7e8de58b49c43b47adb286e43148dade5d0c146f
zhangqd <zhangqd@digiwin.com>
2024-08-21 18:23:36 +0800

    feat: 请求并发修改

--- src/views/concurrenceRequest/MultiRequest.vue
+++ src/views/concurrenceRequest/MultiRequest.vue
@@ -7,22 +7,24 @@
 <script setup>
 import { LimitPromise } from "./limitClass";
 import {useLimitRequest} from './limitRequest'
-const {call} = useLimitRequest()
+const {call} = useLimitRequest(3)
 function sleep(sec) {
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        console.log("sleep" + sec + "秒");
-        resolve();
-      }, sec * 1000);
-    });
-  }
-function requestHook() {
-  call(sleep, 1); // 1秒后打印
-  call(sleep, 2); // 2秒后打印
-  call(sleep, 3); // 3秒后打印
-  call(sleep, 4); // 5秒后打印 1 + 4
-  call(sleep, 5); // 7秒后打印 2 + 5
-  call(sleep, 6); // 9秒后打印 3 + 6
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      console.log("sleep" + sec + "秒");
+      resolve();
+    }, sec * 1000);
+  });
+}
+function limitRequestList(){
+  const list = [1, 2, 3, 4, 5, 6];
+  const pList = list.map(item=>call(sleep,item))
+  return Promise.all(pList);
+}
+ async function requestHook() {
+  await limitRequestList()
+  console.log('end');
+  
 }
 function request() {
   let limitP = new LimitPromise(3);
